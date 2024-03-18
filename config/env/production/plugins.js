e5591036a09295c7419d7ec083465d31c4a80ce8
yasir aslam <yasir7942@outlook.com>
2024-03-18 13:38:14 +0500

    add nodeemail

---
+++ config/env/production/plugins.js
@@ -0,0 +1,23 @@
+module.exports = ({ env }) => ({
+    // ...
+    email: {
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          host: env('SMTP_HOST', 'smtp.gmail.com'),
+          port: env('SMTP_PORT', 587),
+          auth: {
+            user: env('SMTP_USERNAME'),
+            pass: env('SMTP_PASSWORD'),
+          },
+          secure:true,
+          // ... any custom nodemailer options
+        },
+        settings: {
+          defaultFrom: env('DEFAULT_FORM'),
+          defaultReplyTo: env('DEFAULT_REPLYTO'),
+        },
+      },
+    },
+    // ...
+  });
